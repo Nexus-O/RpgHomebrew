a6481ace7e85ecaf20822749595be2e722f488e9
meozofcw <82097608+meozofcw@users.noreply.github.com>
2026-04-20 14:18:01 -0300

    CUCUC

--- rpg-labs/app/firebase.ts
+++ rpg-labs/app/firebase.ts
@@ -2,12 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "SUA_KEY",
-  authDomain: "SEU_DOMAIN",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_BUCKET",
-  messagingSenderId: "SEU_ID",
-  appId: "SEU_APP_ID",
+  apiKey: "AIzaSyB05qSKADcpSxtLiRpYdZ-fE_K7-cZvoi4",
+  authDomain: "nexus-carmesin.firebaseapp.com",
+  databaseURL: "https://nexus-carmesin-default-rtdb.firebaseio.com",
+  projectId: "nexus-carmesin",
+  storageBucket: "nexus-carmesin.firebasestorage.app",
+  messagingSenderId: "678117680506",
+  appId: "1:678117680506:web:ede3aff84beb3767f162c3",
+  measurementId: "G-XJRK70S0J4"
 };
 
 const app = initializeApp(firebaseConfig);
