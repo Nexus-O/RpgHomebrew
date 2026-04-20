fc983f9ee08731a61af20f56425d5e05b1460673
meozofcw <82097608+meozofcw@users.noreply.github.com>
2026-04-20 10:54:35 -0300

    Direito

--- rpg-labs/app/page.tsx
+++ rpg-labs/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
+import Herosection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="bg-black min-h-screen">
       <Navbar />
+      <Herosection />
     </main>
   );
 }
