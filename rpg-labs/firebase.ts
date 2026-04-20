9f7b3794e9207d50bd4b4e20d6ff9ac086c56deb
meozofcw <82097608+meozofcw@users.noreply.github.com>
2026-04-20 14:11:18 -0300

    CUCUCUC

--- rpg-labs/firebase.ts
+++ rpg-labs/firebase.ts
@@ -1,1 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "SUA_KEY",
+  authDomain: "SEU_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_BUCKET",
+  messagingSenderId: "SEU_ID",
+  appId: "SEU_APP_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
