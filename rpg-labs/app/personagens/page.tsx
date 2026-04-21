a760edfe9b00754e37dd1332d488b3fa1e5cecbf
Heizou <diogenesmello206@gmail.com>
2026-04-21 15:26:05 -0300

    MINECRAFT

--- rpg-labs/app/personagens/page.tsx
+++ rpg-labs/app/personagens/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 export const dynamic = "force-dynamic";
+export const ssr = false;
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { onAuthStateChanged } from "firebase/auth";
