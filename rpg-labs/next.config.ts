90f6aa54b274d52d18844b46ca568a0c042a6c3e
Heizou <diogenesmello206@gmail.com>
2026-06-27 20:33:58 -0300

    BIXO BURRO

--- rpg-labs/next.config.ts
+++ rpg-labs/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-  /* config options here */
-=======
->>>>>>> 25e6c2ef78c4dc99a3b0d65151a5d1d4a9f77181
+
 };
 
 export default nextConfig;
